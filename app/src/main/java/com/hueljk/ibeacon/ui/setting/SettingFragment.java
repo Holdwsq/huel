cab778dcb25a3d5bb62c7eed5ffb34d3c9ca97ed
LXH <569884002@qq.com>
2017-02-06 11:20:42 +0800

    设置界面demo

--- app/src/main/java/com/hueljk/ibeacon/ui/setting/SettingFragment.java
+++ app/src/main/java/com/hueljk/ibeacon/ui/setting/SettingFragment.java
@@ -15,10 +15,12 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.hueljk.ibeacon.R;
 import com.hueljk.ibeacon.ui.BaseFragment;
 
+import java.security.PublicKey;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -35,6 +37,7 @@ public class SettingFragment extends BaseFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return  inflater.inflate(R.layout.fragment_setting, container, false);
+
     }
 
 
@@ -43,6 +46,15 @@ public class SettingFragment extends BaseFragment {
         super.initView(view);
         mListView = (ListView)view.findViewById(R.id.set_listview);
         mListView.setAdapter(new ArrayAdapter<String>(getContext(), android. R.layout.simple_list_item_1,mItems ));
+//listview 点击事件
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Toast.makeText(getContext(),"listview被点击了，位置是"+position,Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 
 
