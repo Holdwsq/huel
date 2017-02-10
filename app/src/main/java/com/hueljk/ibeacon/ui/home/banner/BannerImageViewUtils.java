b35a3327ac3bc7cc804fad61c07bc11b133a05e7
春花的粉丝 <1095393288@qq.com>
2017-02-10 16:04:02 +0800

    2-10完善4

---
+++ app/src/main/java/com/hueljk/ibeacon/ui/home/banner/BannerImageViewUtils.java
@@ -0,0 +1,30 @@
+package com.hueljk.ibeacon.ui.home.banner;
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.widget.ImageView;
+
+import com.hueljk.ibeacon.R;
+
+
+/**
+ * ImageView创建工厂
+ */
+public class BannerImageViewUtils {
+
+	/**
+	 * 获取ImageView视图的同时加载显示url
+	 * 
+	 * @return
+	 */
+	public static ImageView getImageView(Context context, String url) {
+		ImageView imageView = (ImageView)LayoutInflater.from(context).inflate(R.layout.view_banner, null);
+//		ImageLoader.getInstance().displayImage(url, imageView);
+		return imageView;
+	}
+
+	public static ImageView getImageView(Context context) {
+		return (ImageView)LayoutInflater.from(context).inflate(R.layout.view_banner, null);
+	}
+}
